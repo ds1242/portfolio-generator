a6b6c595a9bb0b4806e89f629dbb58fdbf0cc3a1
dshaw1242 <david.shaw1242@gmail.com>
2021-05-03 20:46:38 -0600

    updated user input

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -140,7 +140,11 @@ const promptProject = portfolioData => {
 
 promptUser()
     .then(promptProject)
-    .then(projectData => {
-        console.log(projectData)
+    .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
+
+        // fs.writeFile('./index.html',pageHTML, err => {
+        //     if(err) throw new Error(err);
+        // })
     });
     
